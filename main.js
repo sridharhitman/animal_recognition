9ec8edde6308f204ef8a1d9babbdb3b20981fb3b
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2022-03-25 15:01:48 +0530

    part -2, 3 complete

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ function startClassification(){
 function modelReady(){
     classifier.classify(gotResults);
 }
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results)
+        r = Math.floor(Math.random() * 255) + 1;
+        g = Math.floor(Math.random() * 255) + 1;
+        b = Math.floor(Math.random() * 255) + 1;
+        document.getElementById("result_label").innerHTML = "Detected Voice - " + results[0].label;
+        document.getElementById("result_label").style.color = "rgb(" + r +","+ g + "," + b+ ")";
+        display_img = document.getElementById("display_img");
+        if(results[0].label == "clap"){
+            display_img.src= "cat.jpg";
+        }
+        else if(results[0].label == "snap"){
+            display_img.src= "dog.jpg";
+        }
+        else  {
+            display_img.src= "sound.png";
+        }
+    }
+}
